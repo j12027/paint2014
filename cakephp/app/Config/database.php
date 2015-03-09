1957b39878648985ee765f07477af4cbcff883ed
j12027 <j12027@sangi.jp>
2015-03-09 21:24:39 +0900

    20150309

---
+++ cakephp/app/Config/database.php
@@ -0,0 +1,21 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'user',
+		'password' => 'password',
+		'database' => 'test_database_name',
+	);
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'j12027',
+		'password' => 'j12027',
+		'database' => 'j12027',
+		'encoding' => 'utf8'
+	);
+}
